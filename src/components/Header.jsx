87a2ea60bd56d9a0413a5b051a42e4622b391d8e
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-04 15:11:05 +0300

    new edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,13 @@ const Header = () => {
     <header className='w-full h-[800px] bg-fuchsia-400'>
       <div className='container'>
         <NavBar />
+        <div className='flex flex-col items-center max-w-[540px] text-center mx-auto mt-36'>
+          <h1 className='text-8xl text-white'>Your future is electric</h1>
+          <p className='w-[293px] text-center text-xl text-white mt-10'>
+            Get your own ebike or emoped with our flexible subscription
+          </p>
+          <button className='button mt-14'>Start now</button>
+        </div>
       </div>
     </header>
   )
