0b62be52bba019fcbba88ee5fc0a74cbb3e5fad7
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-04 16:59:31 +0300

    new component

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Works from "./Works"
+
+const Main = () => {
+  return (
+    <main>
+      <div className='container'>
+        <Works />
+      </div>
+    </main>
+  )
+}
+
+export default Main
