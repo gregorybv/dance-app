7df35f8e80389e3027407f7e57a3f73ba90335f5
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-04 18:16:44 +0300

    new edits

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import works from "../assets/works.png"
 
 const Works = () => {
   return (
-    <section className='flex justify-between pl-32 mt-16 mb-[73px]'>
+    <section className='max-w-[1280px] w-full mx-auto flex justify-between pl-32 mt-16 mb-[73px]'>
       <div className=''>
         <h2 className='text-6xl'>How it works</h2>
         <ul className='mt-[71px]'>
@@ -12,9 +12,7 @@ const Works = () => {
           <li className='text-2xl'>3. Delivery to your door</li>
           <li className='text-2xl'>4. Ride freely</li>
         </ul>
-        <button className='button bg-blue-500 mt-[51px] '>
-          Start now
-        </button>
+        <button className='button bg-blue-500 mt-[51px] '>Start now</button>
       </div>
       <img src={works} alt='works' />
     </section>
