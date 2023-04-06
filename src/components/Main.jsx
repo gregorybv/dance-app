94b2ec344b0d2104ad341f545c187c18a65ce268
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-06 09:49:56 +0300

    new connect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Works from "./Works"
 import JustRide from "./JustRide"
+import Charge from "./Charge"
 
 const Main = () => {
   return (
     <main>
       <Works />
       <JustRide />
+      <Charge />
     </main>
   )
 }
